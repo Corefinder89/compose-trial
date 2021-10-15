44589aa22d906612a08ae8a682a616ac83d8d232
Corefinder89 <soumyajit.basu1988@outlook.com>
2021-10-15 20:12:36 +0530

    Adding in changes to master branch

--- tests/google_test.py
+++ tests/google_test.py
@@ -2,14 +2,23 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.options import Options
 
+import platform
+
 def test_site():
     try:
         chrome_options = Options()
         chrome_options.add_argument("--headless")
-        driver = webdriver.Chrome(
-            executable_path="driver/chromedriver",
-            options=chrome_options
-        )
+        driver = None
+        if platform.system() == "darwin":
+            driver = webdriver.Chrome(
+                executable_path="driver/macos/chromedriver",
+                options=chrome_options
+            )
+        if platform.system() == "linux":
+            driver = webdriver.Chrome(
+                executable_path="driver/linux/chromedriver",
+                options=chrome_options
+            )
         driver.get("https://www.google.com")
         title = driver.title
         assert title == "Google"
